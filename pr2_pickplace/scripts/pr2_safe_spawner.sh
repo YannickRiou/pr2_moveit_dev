4800d6f5a18dfc43da4c9a9cf1b2c0172cebcb08
Yannick Riou <yannick.riou@laas.fr>
2020-01-28 08:41:58 +0100

    Cleaned repo

--- pr2_pickplace/scripts/pr2_safe_spawner.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# This script safely launches ros nodes with buffer time to allow param server population
-x-terminal-emulator -e roslaunch pr2_pickplace pick_place_demo.launch & sleep 10 &&
-x-terminal-emulator -e roslaunch pr2_moveit_newurdf pr2_moveit_newurdf.launch & sleep 20 &&
-x-terminal-emulator -e rosrun pr2_pickplace pr2_motion
